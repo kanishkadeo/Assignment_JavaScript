a28d45ac4379a7e704248652ff4b7fdc919652a5
kanishkadeo <kanishka.deogade@kennect.io>
2023-01-17 11:35:48 +0530

    Bitwise operation

---
+++ Js1_Bitwise_Operation/Bitwise_opr.js
@@ -0,0 +1,23 @@
+function bitwiseAND(n1, n2) {
+    console.log(`Binary value of n1 :- ${n1.toString(2)}`);
+    console.log(`Binary value of n1 :- ${n2.toString(2)}`);
+    console.log(`Bitwise AND of ${n1} AND ${n2} in Binary is ${(n1 & n2).toString(2)} And in Decimal is ${n1 & n2}`);
+
+    //return (n1 & n2);
+}
+
+function bitwiseOR(n1, n2) {
+    console.log(`Binary value of n1 :- ${n1.toString(2)}`);
+    console.log(`Binary value of n1 :- ${n2.toString(2)}`);
+    console.log(`Bitwise OR of ${n1} AND ${n2} in Binary is ${(n1 | n2).toString(2)} And in Decimal is ${n1 | n2}`);
+
+    //return (n1 & n2);
+}
+
+function bitwiseXor(n1, n2) {
+    console.log(`Binary value of n1 :- ${n1.toString(2)}`);
+    console.log(`Binary value of n1 :- ${n2.toString(2)}`);
+    console.log(`Bitwise XOR of ${n1} AND ${n2} in Binary is ${(n1 ^ n2).toString(2)} And in Decimal is ${n1 ^ n2}`);
+
+    // return (n1 & n2);
+}
